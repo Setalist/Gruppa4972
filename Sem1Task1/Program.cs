89b89ad077157ce5634af291c419ffcd9274eff3
Andrey Maksimovich <andrey.vasilkov.98.av@gmail.com>
2023-09-17 23:55:56 +0500

    solution from sem1 task 0, 1, 3, 5

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №1 
+// Напишите программу, которая на вход принимает 
+// два числа и проверяет, является ли первое число 
+// квадратом второго.
+
+
+Console.WriteLine("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+
+if(num1 == Math.Pow(num2, 2))
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача №3 
+// Напишите программу, которая будет выдавать 
+// название дня недели по заданному номеру.
+
+
+Console.WriteLine("Введите день недели");
+int day = int.Parse(Console.ReadLine()??"0");
+string[] dayOfWeek = new string[7];
+dayOfWeek[0] = "Понедельник";
+dayOfWeek[1] = "Вторник";
+dayOfWeek[2] = "Среда";
+dayOfWeek[3] = "Четверг";
+dayOfWeek[4] = "Пятница";
+dayOfWeek[5] = "Субота";
+dayOfWeek[6] = "Воскресенье";
+if(day > 7)
+{
+    Console.WriteLine("Нет такого дня недели");
+}
+else
+{
+    Console.WriteLine(dayOfWeek[day - 1]);
+}
+
+
+// string day = Console.ReadLine()??"0";
+
+// switch (day)
+// {
+//     case "1": Console.WriteLine("Понедельник"); break;
+//     case "2": Console.WriteLine("Вторник"); break;
+//     case "3": Console.WriteLine("Среда"); break;
+//     case "4": Console.WriteLine("Четверг"); break;
+//     case "5": Console.WriteLine("Пятница"); break;
+//     case "6": Console.WriteLine("Суббота"); break;
+//     case "7": Console.WriteLine("Воскресенье"); break;
+//     default: Console.WriteLine("Неверные данные"); break;
+// }
+
+
+// Console.WriteLine("Enter day number");
+// int dayNum = int.Parse(Console.ReadLine());
+
+// string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(dayNum));
+
+// Console.WriteLine(outDayOfWeek);
