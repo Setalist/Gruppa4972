14d2aaa7866ce4f5c291e7f9943fb8a00757076f
Andrey Maksimovich <andrey.vasilkov.98.av@gmail.com>
2023-09-29 16:09:26 +0500

    solution from sem 2 task 8, 11, 12, 14, 16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача №11 
+// Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого 
+// числа.
+
+//создаем рандомайзер
+int num = new Random().Next(100, 1000);
+//выводим рандомное число
+Console.WriteLine(num);
+//получаем первую цифру
+int firstDigit = num / 100;
+//получаем вторую цифру
+int lastDigit = num % 10;
+//получаем результат
+num = firstDigit * 10 + lastDigit;
+//выводим результат
+Console.WriteLine(num);
+
+
+
+// int ran = new System.Random().Next(100, 1000);
+// System.Console.WriteLine(ran);
+// char[] digitChar = ran.ToString().ToCharArray();
+// System.Console.Write(digitChar[0]);
+// System.Console.Write(digitChar[2]);
+
+
+// Console.Write("Введите число: ");
+// string stringNum = Console.ReadLine()??"0";
+// int num = Convert.ToInt32(stringNum);
+// if (stringNum.Length != 3)
+//     Console.WriteLine("Число не трёхзначное");
+// else
+// {
+//     Console.WriteLine((num / 100) * 10 + (num % 10));
+// }
+// // Console.WriteLine(stringNum.Length != 3 ? "Число не трёхзначное" : (num / 100) * 10 + (num % 10));
+// Console.WriteLine();
+
---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача №12 
+// Напишите программу, которая будет принимать на 
+// вход два числа и выводить, является ли второе 
+// число кратным первому. Если второе число 
+// некратно первому, то программа выводит остаток от 
+// деления. 
+
+        // Вводим 2 числа
+Console.WriteLine("Введите первое число");
+int numA=int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите первое число");
+int numB=int.Parse(Console.ReadLine()??"0");
+int res = numA%numB; // проверяем остаток от деления
+if (res == 0)
+{               // если нет остатка от деления
+    Console.WriteLine("Второе кратно первому");
+}
+else
+{               //если есть остаток от деления
+    Console.WriteLine("Второе не кратно первому. "+"Остаток от деления: "+numA%numB);
+}
+
+// int f = int.Parse(Console.ReadLine()??"0");
+// int s = int.Parse(Console.ReadLine()??"0");
+
+// Console.WriteLine((s%f==0)?true:s%f);
+
+
+// Console.WriteLine("Введите первое число");
+// int num1 = int.Parse(Console.ReadLine()??"0");
+
+// Console.WriteLine("Введите второе число");
+// int num2 = int.Parse(Console.ReadLine()??"0");
+
+// Console.WriteLine(num2%num1==0?"Второе число кратно первому":"Остаток от деления: " + (num2%num1));
